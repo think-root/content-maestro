c6095ee9cf5b32c4dcd752e1bbd7d268e37688e7
sigmanor <sigmanor@gmail.com>
2025-03-04 14:05:38 +0200

    refactor: update scheduler to use new schedule package
    - Replace the old scheduler package with the new schedule package
    - Update the cron jobs to use the new package functions
    - Remove unused log import

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,39 +8,41 @@ import (
 	"github.com/joho/godotenv"
 
 	"content-maestro/internal/logger"
-	"content-maestro/internal/scheduler"
+	"content-maestro/internal/schedule"
 	"content-maestro/internal/utils"
 )
+
+var log = logger.NewLogger()
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	log.Debug("environment loaded successfully")
 	if os.Getenv("APP_VERSION") == "" {
 		os.Setenv("APP_VERSION", "dev")
 	}
 }
 
 func main() {
-	log := logger.NewLogger()
-	defer log.Sync()
-
 	utils.CreateDirIfNotExist("tmp/gh_project_img")
 
 	log.Debug("content-maestro application starting...")
 	log.Debug("APP_VERSION:", os.Getenv("APP_VERSION"))
-	log.Debug("environment loaded successfully")
-	log.Debug("starting scheduler...")
+	log.Debug("starting schedules...")
 
-	s := scheduler.StartCron()
+	s1 := schedule.MessageCron()
+	s2 := schedule.CollectCron()
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	sig := <-sigChan
+
 	log.Debug("received signal: ", sig)
 	log.Debug("shutting down...")
 
-	s.Stop()
+	s1.Stop()
+	s2.Stop()
 }
