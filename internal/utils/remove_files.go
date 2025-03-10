b69ccc873057728de934e333e741a4a870296133
sigmanor <sigmanor@gmail.com>
2025-03-10 15:04:34 +0200

    refactor: add error handling for non-existent directory in RemoveAllFilesInFolder
    - Add check for directory existence
    - Return error if directory does not exist

--- internal/utils/remove_files.go
+++ internal/utils/remove_files.go
@@ -1,21 +1,28 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func RemoveAllFilesInFolder(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return fmt.Errorf("directory %s does not exist", dir)
+	}
+
 	files, err := filepath.Glob(filepath.Join(dir, "*"))
 	if err != nil {
 		return err
 	}
+
 	for _, file := range files {
 		err = os.Remove(file)
 		if err != nil {
 			return err
 		}
 	}
+
 	log.Debug("Files removed successfully!")
 	return nil
 }
