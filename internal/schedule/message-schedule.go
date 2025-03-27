7ef05e4dc3e65cd64745f1df181edf675c1b5393
sigmanor <sigmanor@gmail.com>
2025-03-27 14:00:42 +0200

    refactor: move message job logic to separate function
    This change refactors the message cron job logic into a separate function `MessageJob` to improve code organization and readability. The `MessageCron` function now only handles scheduling and error checking.

--- internal/schedule/message-schedule.go
+++ internal/schedule/message-schedule.go
@@ -1,7 +1,6 @@
 package schedule
 
 import (
-	"content-maestro/internal/api"
 	"content-maestro/internal/logger"
 	"content-maestro/internal/repository"
 	"content-maestro/internal/socialify"
@@ -15,101 +14,49 @@ import (
 
 var log = logger.NewLogger()
 
-func MessageCron(store *store.Store) *gocron.Scheduler {
-	s := gocron.NewScheduler(time.UTC)
+func MessageJob(s *gocron.Scheduler) {
+	log.Debug("cron job started")
 
-	setting, err := store.GetCronSetting("message")
+	repo, err := repository.GetRepository(1, false, "ASC", "date_added")
 	if err != nil {
-		log.Error("Error getting cron setting: %v", err)
-		return s
+		log.Error("Error getting repository: %v", err)
+		return
 	}
 
-	if setting == nil || !setting.IsActive {
-		log.Debug("Message cron is disabled")
-		return s
-	}
+	item := repo.Data.Items[0]
+	username_repo := strings.TrimPrefix(item.URL, "https://github.com/")
+	image_name := "./tmp/gh_project_img/image.png"
 
-	s.Cron(setting.Schedule).Do(func() {
-		log.Debug("cron job started")
-
-		repo, err := repository.GetRepository(1, false, "ASC", "date_added")
+	err = socialify.Socialify(username_repo)
+	if err != nil {
+		log.Error(err)
+		err := utils.CopyFile("./assets/banner.jpg", image_name)
 		if err != nil {
-			log.Error("Error getting repository: %v", err)
+			log.Error("Failed to copy file: %v", err)
 			return
 		}
+	}
 
-		item := repo.Data.Items[0]
-		username_repo := strings.TrimPrefix(item.URL, "https://github.com/")
-		image_name := "./tmp/gh_project_img/image.png"
+	if _, err := repository.UpdateRepositoryPosted(item.URL, true); err != nil {
+		log.Error("Error updating repository posted status: %v", err)
+	}
 
-		err = socialify.Socialify(username_repo)
-		if err != nil {
-			log.Error(err)
-			err := utils.CopyFile("./assets/banner.jpg", image_name)
-			if err != nil {
-				log.Error("Failed to copy file: %v", err)
-				return
-			}
-		}
+	err = utils.RemoveAllFilesInFolder("./tmp/gh_project_img")
+	if err != nil {
+		log.Error(err)
+	}
+}
 
-		err = api.LoadAPIConfigs("./internal/api/apis-config.yml")
-		if err != nil {
-			log.Error("Failed to load API configurations: %v", err)
-		}
+func MessageCron(store *store.Store) *gocron.Scheduler {
+	setting, err := store.GetCronSetting("message")
+	if err != nil || setting == nil || !setting.IsActive {
+		log.Debug("Message cron is disabled")
+		return gocron.NewScheduler(time.UTC)
+	}
 
-		for apiName, endpoint := range api.GetAPIConfigs().APIs {
-			if !endpoint.Enabled {
-				continue
-			}
-
-			var req api.RequestConfig
-
-			commonFields := map[string]string{
-				"text": item.Text,
-				"url":  item.URL,
-			}
-
-			switch strings.ToLower(endpoint.ContentType) {
-			case "multipart":
-				req = api.RequestConfig{
-					APIName:    apiName,
-					FormFields: commonFields,
-					FileFields: map[string]string{
-						"image": image_name,
-					},
-				}
-			case "json":
-				req = api.RequestConfig{
-					APIName:  apiName,
-					JSONBody: map[string]interface{}{"text": item.Text, "url": item.URL},
-				}
-			default:
-				req = api.RequestConfig{
-					APIName:  apiName,
-					JSONBody: map[string]interface{}{"text": item.Text, "url": item.URL},
-				}
-			}
-
-			resp, err := api.ExecuteRequest(req)
-			if err != nil {
-				log.Errorf("%s API error: %v", apiName, err)
-			} else if resp.Success {
-				log.Debugf("%s post created successfully!", apiName)
-			}
-		}
-
-		if _, err := repository.UpdateRepositoryPosted(item.URL, true); err != nil {
-			log.Error("Error updating repository posted status: %v", err)
-		}
-
-		err = utils.RemoveAllFilesInFolder("./tmp/gh_project_img")
-		if err != nil {
-			log.Error(err)
-		}
-	})
-
+	s := gocron.NewScheduler(time.UTC)
+	s.Cron(setting.Schedule).Do(MessageJob, s)
 	s.StartAsync()
-	time.Sleep(time.Second)
 	log.Debug("scheduler started successfully")
 	return s
 }
