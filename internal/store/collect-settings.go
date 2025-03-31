20051e3013a07f1b97fdfbe6037feaa5aeae3eb1
sigmanor <sigmanor@gmail.com>
2025-03-31 13:45:54 +0300

    feat(store): add collect settings management
    This commit adds functionality to manage collect settings in the store. It includes methods to initialize default settings, retrieve current settings, and update settings. The settings are stored in a BadgerDB database using JSON encoding.

---
+++ internal/store/collect-settings.go
@@ -0,0 +1,55 @@
+package store
+
+import (
+	"encoding/json"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+type CollectSettings struct {
+	MaxRepos           int    `json:"max_repos"`
+	Since              string `json:"since"`
+	SpokenLanguageCode string `json:"spoken_language_code"`
+}
+
+func (s *Store) InitializeDefaultCollectSettings() error {
+	settings := CollectSettings{
+		MaxRepos:           5,
+		Since:              "daily",
+		SpokenLanguageCode: "en",
+	}
+	return s.db.Update(func(txn *badger.Txn) error {
+		data, err := json.Marshal(settings)
+		if err != nil {
+			return err
+		}
+		return txn.Set([]byte("collect_settings"), data)
+	})
+}
+
+func (s *Store) GetCollectSettings() (*CollectSettings, error) {
+	var settings CollectSettings
+	err := s.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("collect_settings"))
+		if err != nil {
+			return err
+		}
+		return item.Value(func(val []byte) error {
+			return json.Unmarshal(val, &settings)
+		})
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &settings, nil
+}
+
+func (s *Store) UpdateCollectSettings(settings *CollectSettings) error {
+	return s.db.Update(func(txn *badger.Txn) error {
+		data, err := json.Marshal(settings)
+		if err != nil {
+			return err
+		}
+		return txn.Set([]byte("collect_settings"), data)
+	})
+}
