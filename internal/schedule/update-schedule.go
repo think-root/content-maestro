79ee3344429dcb6f01bedb54d50b42c696cc2314
sigmanor <sigmanor@gmail.com>
2025-06-12 19:49:39 +0300

    refactor(schedule): simplify cron job initialization
    Replaces direct calls to specific cron functions with a generic NewScheduler call,
    centralizing cron job creation and configuration. This change improves maintainability
    and prepares for more dynamic schedule management.

--- internal/schedule/update-schedule.go
+++ internal/schedule/update-schedule.go
@@ -6,12 +6,12 @@ import (
 	"github.com/go-co-op/gocron"
 )
 
-func UpdateScheduler(cronName string, store *store.Store) *gocron.Scheduler {
+func UpdateScheduler(cronName string, store store.StoreInterface) *gocron.Scheduler {
 	switch cronName {
 	case "message":
-		return MessageCron(store)
+		return NewScheduler(store, cronName, "12 12 * * *")
 	case "collect":
-		return CollectCron(store)
+		return NewScheduler(store, cronName, "13 13 * * 6")
 	default:
 		return nil
 	}
