a2ba2c4bacfd05f47ef9ffa737ecb719d21a8be9
sigmanor <sigmanor@gmail.com>
2025-06-13 19:21:11 +0300

    feat(store): add prompt settings persistence methods
    Adds `GetPromptSettings` and `UpdatePromptSettings` methods to the PostgresStore. These methods allow for retrieving and updating the application's prompt settings from the `think_prompt` table in the database. This enables the API to manage prompt configurations.

---
+++ internal/store/prompt-settings.go
@@ -0,0 +1,57 @@
+package store
+
+import (
+	"content-maestro/internal/models"
+	"fmt"
+	"time"
+)
+
+func (s *PostgresStore) GetPromptSettings() (*models.PromptSettings, error) {
+	var settings models.PromptSettings
+	err := s.db.QueryRow(`
+		SELECT use_direct_url, llm_provider, temperature, content, updated_at
+		FROM think_prompt
+		WHERE id = 1
+	`).Scan(&settings.UseDirectURL, &settings.LlmProvider, &settings.Temperature, &settings.Content, &settings.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &settings, nil
+}
+
+func (s *PostgresStore) UpdatePromptSettings(settings *models.UpdatePromptSettingsRequest) error {
+	query := `
+		UPDATE think_prompt 
+		SET updated_at = $1`
+	args := []interface{}{time.Now()}
+	argIndex := 2
+	
+	if settings.UseDirectURL != nil {
+		query += fmt.Sprintf(", use_direct_url = $%d", argIndex)
+		args = append(args, *settings.UseDirectURL)
+		argIndex++
+	}
+	
+	if settings.LlmProvider != nil {
+		query += fmt.Sprintf(", llm_provider = $%d", argIndex)
+		args = append(args, *settings.LlmProvider)
+		argIndex++
+	}
+	
+	if settings.Temperature != nil {
+		query += fmt.Sprintf(", temperature = $%d", argIndex)
+		args = append(args, *settings.Temperature)
+		argIndex++
+	}
+	
+	if settings.Content != nil {
+		query += fmt.Sprintf(", content = $%d", argIndex)
+		args = append(args, *settings.Content)
+		argIndex++
+	}
+	
+	query += " WHERE id = 1"
+	
+	_, err := s.db.Exec(query, args...)
+	return err
+}
