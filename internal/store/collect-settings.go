1303e14338e7a1f85ce98f89bee22b5281265b29
sigmanor <sigmanor@users.noreply.github.com>
2026-01-09 13:18:59 +0200

    feat(store): add resource, period, and language to CollectSettings
    Added new fields to CollectSettings struct to support additional filtering options.

--- internal/store/collect-settings.go
+++ internal/store/collect-settings.go
@@ -2,6 +2,9 @@ package store
 
 type CollectSettings struct {
 	MaxRepos           int    `json:"max_repos"`
+	Resource           string `json:"resource"`
 	Since              string `json:"since"`
 	SpokenLanguageCode string `json:"spoken_language_code"`
+	Period             string `json:"period"`
+	Language           string `json:"language"`
 }
